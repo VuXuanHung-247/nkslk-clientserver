771bdf5a5b116cb3b5745ec16cc47e2b40c50e6a
vmquanglc <vmquanglc@gmail.com>
2021-11-07 22:18:01 +0700

    commit by QuangStar

--- api/NKSLK.API/NKSLK.API/Repository/TasksRepository.cs
+++ api/NKSLK.API/NKSLK.API/Repository/TasksRepository.cs
@@ -92,5 +92,41 @@ namespace NKSLK.API.Repository
                 return rowEffect;
             }
         }
+        public IEnumerable<Tasks> GetTasksMinPrice()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var dataParam = new DynamicParameters();
+                var obj = connection.Query<Tasks>(sql: "[Proc_GetTasksMinPrice]", param: dataParam, commandType: CommandType.StoredProcedure).ToList();
+                return obj;
+            }
+        }
+        public IEnumerable<Tasks> GetTasksMaxPrice()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var dataParam = new DynamicParameters();
+                var obj = connection.Query<Tasks>(sql: "[Proc_GetTasksMaxPrice]", param: dataParam, commandType: CommandType.StoredProcedure).ToList();
+                return obj;
+            }
+        }
+        public IEnumerable<Tasks> GetTasksLessAVG()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var dataParam = new DynamicParameters();
+                var obj = connection.Query<Tasks>(sql: "[Proc_GetTasksLessAVG]", param: dataParam, commandType: CommandType.StoredProcedure).ToList();
+                return obj;
+            }
+        }
+        public IEnumerable<Tasks> GetTasksBiggerAVG()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var dataParam = new DynamicParameters();
+                var obj = connection.Query<Tasks>(sql: "[Proc_GetTasksBiggerAVG]", param: dataParam, commandType: CommandType.StoredProcedure).ToList();
+                return obj;
+            }
+        }
     }
 }
